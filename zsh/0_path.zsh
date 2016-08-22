67bfe8c7cc979c20ecb6c4e40e1111e08ff1dfae
Michael Aaron <michalaaron@nestlabs.com>
2016-08-22 14:15:18 -0600

    added some paths

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,11 @@
 # path, the 0 in the filename causes this to load first
 path=(
+  $HOME/OpenSource/depot_tools
+  /usr/local/mysql/bin
   $path
   $HOME/.yadr/bin
   $HOME/.yadr/bin/yadr
+  /Library/Frameworks/Python.framework/Versions/2.7/bin
+  $Home/bin
 )
 
